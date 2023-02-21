87b39c2fab71eb33c7f2ad3018fabb9d527685d0
ccarver80 <ckc_80@outlook.com>
2023-02-21 16:13:11 -0600

    itit

--- next-app/pages/_app.js
+++ next-app/pages/_app.js
@@ -1,5 +1,9 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
