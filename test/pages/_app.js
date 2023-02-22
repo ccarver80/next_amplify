bc800de8667cd945011a42b9a6d1fc0587d02f89
ccarver80 <ckc_80@outlook.com>
2023-02-21 18:19:36 -0600

    configure

--- test/pages/_app.js
+++ test/pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { Amplify } from "aws-amplify";
+
+import awsconfig from "../src/aws-exports";
+
+Amplify.configure({ ...awsconfig, ssr: true });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
